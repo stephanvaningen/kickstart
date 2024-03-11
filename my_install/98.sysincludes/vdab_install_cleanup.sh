a4011e3cfb810384afb605af3e4e4b5af00074e8
Stephan van Ingen <stephanvaningen@gmail.com>
2024-03-11 18:39:06 +0100

    Create vdab_install_cleanup.sh

---
+++ my_install/98.sysincludes/vdab_install_cleanup.sh
@@ -0,0 +1,9 @@
+
+
+#manual list of all we want to get rid of. Review before release.
+rm -Rf ${install_root}/50.configs
+rm -Rf ${install_root}/70.todo
+rm -Rf ${install_root}/80.wip
+rm -Rf ${install_root}/98.sysincludes
+rm -Rf ${install_root}/90.done
+rm -f ${install_root}/install*.sh
